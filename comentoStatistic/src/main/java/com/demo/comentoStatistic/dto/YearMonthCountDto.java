eed595b0db4908b8763c0097ad32695727ec4326
김동오 <dongo1450@gmail.com>
2025-02-13 21:22:06 +0900

    hi

---
+++ comentoStatistic/src/main/java/com/demo/comentoStatistic/dto/YearMonthCountDto.java
@@ -0,0 +1,13 @@
+package com.demo.comentoStatistic.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class YearMonthCountDto {
+    String yearMonth;
+    Integer totCnt;
+}
+
